1118a6711194dca55949066a0f5e6ad1186d29e3
phi.le <phi.le@bnksolution.com>
2024-08-14 18:02:42 +0700

    update code

--- components/SideBar/index.vue
+++ components/SideBar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[250px] flex-col items-stretch relative border-b lg:border-b-0 lg:border-r border-gray-200 dark:border-gray-800 lg:w-[--width] flex-shrink-0 hidden lg:flex"
+    class="w-[250px] h-screen flex-col items-stretch relative border-b lg:border-b-0 lg:border-r border-gray-200 dark:border-gray-800 flex-shrink-0 hidden lg:flex"
   >
     <NuxtLink to="/" exact-active-class="active">
       <span>Home page</span>
